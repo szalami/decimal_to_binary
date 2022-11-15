c71ffba4bdc006f896cf88f976b4cfa2cd34d020
szalami <szalami72@gmail.com>
2022-11-15 23:04:17 +0100

    working

--- dec_to_bin.py
+++ dec_to_bin.py
@@ -1,9 +1,18 @@
 
-max = 128
+def setVal(num):
+    temp = 0
+    while pow(2, temp) <= num:          
+        temp = temp + 1
+    return pow(2, temp-1)
+        
+
 num = int(input("Szám: "))
 
-while max >= 1:
-    if (num >= max):
+max = setVal(num)
+
+
+while max >= 1: 
+    if num >= max:
         print(1, end=" ")
         num = num - max
     else:
